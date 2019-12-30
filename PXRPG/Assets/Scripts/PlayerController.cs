b4f59d05ea79d38ffa5d1394086792bd3ca041d6
Daniel <dpar@kth.se>
2019-12-30 13:56:37 +0100

    Character controller added (movement)

---
+++ PXRPG/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 moveDirection;
+    public float moveSpeed;
+    public float jumpForce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveDirection = new Vector3(-1*Input.GetAxis("Horizontal") * moveSpeed, 0f, -1*Input.GetAxis("Vertical") * moveSpeed);
+
+        controller.Move(moveDirection);
+    }
+}
