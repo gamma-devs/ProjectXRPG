8c2f5ed46721720376f40382423c518146fe746b
Daniel <dpar@kth.se>
2020-03-16 20:29:11 +0100

    New enemy and projectile line added

---
+++ PXRPG/Assets/AI&Animations/PurpleArcher/IdleBehaviourPurple.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ *  This is the idle AI for purple archer, this archer will just stand on his place and shoot the player when
+ *  player is in range.
+ */
+public class IdleBehaviourPurple : StateMachineBehaviour
+{
+    PlayerController player;
+    public float alertRadius;
+    //OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+    }
+
+    //OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Vector2 enemyPos = new Vector2(animator.transform.position.x, animator.transform.position.z);
+        Vector2 playerPos = new Vector2(player.transform.position.x, player.transform.position.z);
+        float distance = Vector2.Distance(enemyPos, playerPos);
+        if(distance < alertRadius)
+        {
+            //Set charge to true.
+            animator.SetBool("charge", true);
+        }
+    }
+
+    //OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        
+    }
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
---
+++ PXRPG/Assets/Prefabs/Enemies/Archer/CurveScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ *  Draw a bezier curve line.
+ *  This is used to show enemy trajectory path.
+ */
+public class CurveScript : MonoBehaviour
+{
+    public LineRenderer lr;
+    const int nrPoints = 2;
+    Vector3[] positions = new Vector3[nrPoints];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        lr.useWorldSpace = true;
+    }
+
+    public void draw(Vector3 startPos, Vector3 endPos)
+    {
+
+        positions[0] = startPos;
+        //positions[1] = new Vector3(0.0f, 2.0f, 0.0f);
+        positions[1] = endPos;
+
+        lr.positionCount = nrPoints;
+        lr.SetPositions(positions);
+        lr.widthMultiplier = 0.1f;
+        lr.material = new Material(Shader.Find("Sprites/Default"));
+    }
+
+    public void updatePositions(Vector3 startPos, Vector3 endPos)
+    {
+        lr.SetPosition(0, startPos);
+        lr.SetPosition(1, endPos);
+        //Debug.Log("hej");
+    }
+}
